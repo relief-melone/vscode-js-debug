6723f713cbfc6020dea93ea75927a9e4aaa4313f
Relief Melone <relief.melone@gmail.com>
2025-05-07 20:25:03 +0200

    added buildinputs to buildNodeDependencies

--- node-env.nix
+++ node-env.nix
@@ -595,7 +595,8 @@ let
         buildInputs = [ tarWrapper python nodejs ]
           ++ lib.optional (stdenv.isLinux) utillinux
           ++ lib.optional (stdenv.isDarwin) libtool
-          ++ buildInputs;
+          ++ buildInputs
+          ++ additionalBuildInputs;
 
         inherit dontStrip; # Stripping may fail a build for some package deployments
         inherit dontNpmInstall unpackPhase buildPhase;
